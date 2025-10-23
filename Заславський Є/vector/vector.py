c0bd495ab66f0115cf92de2de6f37e149369f9c0
yeahorik <yehorzaslavskiy@gmail.com>
2025-10-23 18:03:49 +0300

    Update vector.py
    fix code

--- Заславський Є/vector/vector.py
+++ Заславський Є/vector/vector.py
@@ -1,11 +1,12 @@
 def read_vectors(filename):
     with open(filename, "r") as f:
         lines = f.readlines()
-    vectors = []
-    for line in lines:
-        nums = [float(x) for x in line.split()]
-        vectors.append(nums)
-    return vectors
+
+    operation = lines[0].strip()
+    v1 = [float(x) for x in lines[1].split()]
+    v2 = [float(x) for x in lines[2].split()]
+
+    return operation, v1, v2
 
 
 def add_vectors(a, b):
@@ -21,7 +22,7 @@ def mul_vectors(a, b):
 
 
 def div_vectors(a, b):
-    return [a[i] / b[i] if b[i] != 0 else "∞" for i in range(len(a))]
+    return [a[i] / b[i] if b[i] != 0 else "error" for i in range(len(a))]
 
 
 def write_result(filename, text):
@@ -29,36 +30,22 @@ def write_result(filename, text):
         f.write(text)
 
 
+operation, v1, v2 = read_vectors("data.txt")
 
-vectors = read_vectors("data.txt")
+if len(v1) != len(v2):
+    text = "Помилка: вектори різного розміру."
+else:
+    result = None
+    if operation == "+":
+        result = add_vectors(v1, v2)
+    elif operation == "-":
+        result = sub_vectors(v1, v2)
+    elif operation == "*":
+        result = mul_vectors(v1, v2)
+    elif operation == "/":
+        result = div_vectors(v1, v2)
 
-v1 = vectors[0]
-v2 = vectors[1]
+    text = f"{v1} {operation} {v2} = {result}"
 
-print("1. Додавання")
-print("2. Віднімання")
-print("3. Множення")
-print("4. Ділення")
-
-choice = int(input("Оберіть дію: "))
-
-result = None
-operation = ""
-
-if choice == 1:
-    result = add_vectors(v1, v2)
-    operation = "+"
-elif choice == 2:
-    result = sub_vectors(v1, v2)
-    operation = "-"
-elif choice == 3:
-    result = mul_vectors(v1, v2)
-    operation = "*"
-elif choice == 4:
-    result = div_vectors(v1, v2)
-    operation = "/"
-
-text = f"{v1} {operation} {v2} = {result}"
 print(text)
-
-write_result("../result.txt", text)
+write_result("result.txt", text)
