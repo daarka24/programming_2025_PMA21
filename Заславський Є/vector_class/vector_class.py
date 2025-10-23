d578a0b36aa8ef4eff41af92c82542031b89dd8f
yeahorik <yehorzaslavskiy@gmail.com>
2025-10-23 18:06:41 +0300

    Update vector_class.py
    fix code

--- Заславський Є/vector_class/vector_class.py
+++ Заславський Є/vector_class/vector_class.py
@@ -2,17 +2,31 @@ class Vector:
     def __init__(self, coords):
         self.coords = coords
 
+    def __check_size(self, other):
+        if len(self.coords) != len(other.coords):
+            raise ValueError("Різний розмір векторів")
+
     def __add__(self, other):
-        return Vector([a + b for a, b in zip(self.coords, other.coords)])
+        self.__check_size(other)
+        result = [self.coords[i] + other.coords[i] for i in range(len(self.coords))]
+        return Vector(result)
 
     def __sub__(self, other):
-        return Vector([a - b for a, b in zip(self.coords, other.coords)])
+        self.__check_size(other)
+        result = [self.coords[i] - other.coords[i] for i in range(len(self.coords))]
+        return Vector(result)
 
     def __mul__(self, other):
-        return Vector([a * b for a, b in zip(self.coords, other.coords)])
-#zip об’єднує їх поелементно в пари а і б
+        self.__check_size(other)
+        result = [self.coords[i] * other.coords[i] for i in range(len(self.coords))]
+        return Vector(result)
+
     def __truediv__(self, other):
-        return Vector([a / b if b != 0 else None for a, b in zip(self.coords, other.coords)])
+        self.__check_size(other)
+        if any(x == 0 for x in other.coords):
+            raise ValueError("Ділення на нульовий елемент")
+        result = [self.coords[i] / other.coords[i] for i in range(len(self.coords))]
+        return Vector(result)
 
     def __str__(self):
-        return "(" + ",".join(map(str, self.coords)) + ")"
+        return "(" + ", ".join(str(x) for x in self.coords) + ")"
