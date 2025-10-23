8f4c6a1ae36100f5493293c3d1f0886446360d50
Oleksii <oleksii.dashchuk@lnu.edu.ua>
2025-10-23 16:21:42 +0300

    code_update

--- Дащук О/script.py
+++ Дащук О/script.py
@@ -1,27 +1,43 @@
+def check_length_of_matrices(matrix_one, matrix_two):
+    if len(matrix_one) != len(matrix_two):
+        return False
+
+    for i in range(len(matrix_one)):
+        if len(matrix_one[i]) != len(matrix_two[i]):
+            return False
+    return True
+
+
 def mat_plus_mat(matrix_one, matrix_two):
+    if not check_length_of_matrices(matrix_one, matrix_two):
+        print("the length of the matrices must be equal")
+        return
     result = [[0 for _ in range(len(matrix_two[0]))] for _ in range(len(matrix_one))]
     for i in range(len(matrix_one)):
         for j in range(len(matrix_two[0])):
             result[i][j] = matrix_one[i][j] + matrix_two[i][j]
     return result
 
-
 def mat_minus_mat(matrix_one, matrix_two):
+    if not check_length_of_matrices(matrix_one, matrix_two):
+        print("the length of the matrices must be equal")
+        return
     result = [[0 for _ in range(len(matrix_two[0]))] for _ in range(len(matrix_one))]
     for i in range(len(matrix_one)):
         for j in range(len(matrix_two[0])):
             result[i][j] = matrix_one[i][j] - matrix_two[i][j]
     return result
 
-
 def mat_multi_mat(matrix_one, matrix_two):
+    if not check_length_of_matrices(matrix_one, matrix_two):
+        print("the length of the matrices must be equal")
+        return
     result = [[0 for _ in range(len(matrix_two[0]))] for _ in range(len(matrix_one))]
     for i in range(len(matrix_one)):
         for j in range(len(matrix_two[0])):
             for k in range(len(matrix_two)):
                 result[i][j] += matrix_one[i][k] * matrix_two[k][j]
     return result
-
 
 def inverse_matrix(matrix):
     n = len(matrix)
@@ -45,12 +61,14 @@ def inverse_matrix(matrix):
     return [row[n:] for row in augmented]
 
 def divide_matrices(matrix_one, matrix_two):
+    if not check_length_of_matrices(matrix_one, matrix_two):
+        print("the length of the matrices must be equal")
+        return
     inverse_b = inverse_matrix(matrix_one)
     if inverse_b is None:
         return None
     result = mat_multi_mat(matrix_two, inverse_b)
     return result
-
 
 matrix_one = []
 matrix_two = []
@@ -67,33 +85,36 @@ try:
             matrix_two.append([float(x) for x in line.split()])
 except FileNotFoundError:
     print("file_2.txt is not found")
-try:
 
-    with open("result.txt", "w") as file:
-        file.write("Matrix One:\n")
-        for result in matrix_one:
-            file.write(str(result)+"\n")
-        file.write("\nMatrix Two:\n")
-        for result in matrix_two:
-            file.write(str(result) + "\n")
-        file.write("\nAddition:\n")
-        sum=mat_plus_mat(matrix_one, matrix_two)
-        for result in sum:
-            file.write(str(result) + "\n")
-        file.write("\nSubtraction:\n")
-        subtraction_result = mat_minus_mat(matrix_one, matrix_two)
-        for result in subtraction_result:
-            file.write(str(result) + "\n")
-        file.write("\nMultiplication:\n")
-        multiplication_result = mat_multi_mat(matrix_one, matrix_two)
-        for result in multiplication_result:
-            file.write(str(result) + "\n")
-        file.write("\nDivision:\n")
-        division_result = divide_matrices(matrix_one, matrix_two)
-        if division_result:
-            for result in division_result:
-                file.write(str(result) + "\n")
-        else:
-            file.write("Cannot Divide Matrix")
-except Exception as e:
-    print(f"Error writing to file: {e}")
+with open("result.txt", "w") as file:
+    sum_result = mat_plus_mat(matrix_one, matrix_two)
+    file.write("\nAddition:\n")
+    if sum_result:
+        for row in sum_result:
+            file.write(str(row) + "\n")
+    else:
+        file.write("Cannot add matrices\n")
+
+    subtraction_result = mat_minus_mat(matrix_one, matrix_two)
+    file.write("\nSubtraction:\n")
+    if subtraction_result:
+        for row in subtraction_result:
+            file.write(str(row) + "\n")
+    else:
+        file.write("Cannot subtract matrices\n")
+
+    multiplication_result = mat_multi_mat(matrix_one, matrix_two)
+    file.write("\nMultiplication:\n")
+    if multiplication_result:
+        for row in multiplication_result:
+            file.write(str(row) + "\n")
+    else:
+        file.write("Cannot multiply matrices\n")
+
+    division_result = divide_matrices(matrix_one, matrix_two)
+    file.write("\nDivision:\n")
+    if division_result:
+        for row in division_result:
+            file.write(str(row) + "\n")
+    else:
+        file.write("Cannot divide matrices\n")
